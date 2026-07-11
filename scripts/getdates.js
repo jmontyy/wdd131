39a6ed41e5b3df9c1ddb7b8818ef0d189e36b824
jmontyy <joshmontgomerymmiv@gmail.com>
2026-07-11 12:07:32 -0500

    week1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+
+document.getElementById("lastModified").innerHTML  =
+    "Last Modified: " + document.lastModified;
